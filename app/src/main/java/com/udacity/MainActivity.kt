f30fdf59a1d5e4d4564b7d1ec6a8c0b21edb90c3
Juan villarroel <vilfrack21@gmail.com>
2021-01-17 20:23:29 -0300

    Finish dice

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -3,11 +3,15 @@ package com.udacity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,11 +20,20 @@ class MainActivity : AppCompatActivity() {
         rollButon.setOnClickListener {
             rollDice()
         }
+        diceImage= findViewById(R.id.dice_img)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.TextResult)
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 ->R.drawable.dice_2
+            3 ->R.drawable.dice_3
+            4 ->R.drawable.dice_4
+            5 ->R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
